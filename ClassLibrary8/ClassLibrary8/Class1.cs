82afb5af97a3b9b171de3f071a04148b82adfc8e
komiyamma <komiyama0629@gmail.com>
2022-12-21 17:36:37 +0900

    サンプル

---
+++ ClassLibrary8/ClassLibrary8/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace ClassLibrary8
+{
+    public class Class1
+    {
+        [UnmanagedCallersOnly(EntryPoint = "doubleint", CallConvs = new[] { typeof(CallConvCdecl) })]
+        public static nint DoubleInt(nint i) => i * 2;
+
+        static string ret_sum_string = "";
+
+        [UnmanagedCallersOnly(EntryPoint = "sumstring", CallConvs = new[] { typeof(CallConvCdecl) })]
+        public static IntPtr SumString(IntPtr first, IntPtr second)
+        {
+            // Parse strings from the passed pointers 
+            string my1String = Marshal.PtrToStringUni(first) ?? "";
+            string my2String = Marshal.PtrToStringUni(second) ?? "";
+
+            // Concatenate strings 
+            ret_sum_string = my1String + my2String;
+
+            // Assign pointer of the concatenated string to sumPointer
+            IntPtr sumPointer = Marshal.StringToHGlobalUni(ret_sum_string);
+
+            // Return pointer
+            return sumPointer;
+        }
+    }
+}
